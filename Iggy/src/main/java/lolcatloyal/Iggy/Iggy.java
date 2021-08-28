d040128bf0ea8e5bf142400f6694e642b4a584a5
Keith L <klowry2001@gmail.com>
2021-08-28 13:46:17 -0400

    Created Project

---
+++ Iggy/src/main/java/lolcatloyal/Iggy/Iggy.java
@@ -0,0 +1,15 @@
+package lolcatloyal.Iggy;
+
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+
+
+public class Iggy {
+
+    public static JDABuilder builder;
+
+    public static void main(String[] args) throws LoginException {
+
+    }
+}
